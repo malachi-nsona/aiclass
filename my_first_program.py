e6a7239af32dbab653cb03093dfce7f2d1dea940
malachi-nsona <malachinsona49@gmail.com>
2025-08-14 00:39:38 -0700

    very first lesson

---
+++ my_first_program.py
@@ -0,0 +1,16 @@
+name='Malachi Tamiweh Nsona'
+age=18
+income='Fifty Thousand Malawi Kwacha'
+imabillionaire=False
+print(name)
+print(age)
+print(income)
+print('my name is')
+print(name)
+print('my age is')
+print(age)
+print('my money in the account')
+print(income)
+print('i am a billionaire')
+print(imabillionaire)
+
