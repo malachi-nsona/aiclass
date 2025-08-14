d29ba872e620d818aa5fc5ce0cdb65b3bdb0fdec
malachi-nsona <malachinsona49@gmail.com>
2025-08-14 00:43:11 -0700

    trying alone

---
+++ second_day.py
@@ -0,0 +1,19 @@
+name='malachi tamiweh nsona'
+age=18
+gender='female'
+school='st marys secondary'
+print(name)
+print(age)
+print(gender)
+print(school)
+print(f'my name is', name)
+print(f'i am',age)
+print(f'i am',gender)
+print(f'i learnt at',school)
+
+
+def add(num, num2):
+    return num + num2
+
+
+
