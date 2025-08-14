033a02f5c9f9e80ac6c9ec07be7c617551ed28cd
malachi-nsona <malachinsona49@gmail.com>
2025-08-14 00:45:12 -0700

    very first unit conversion

---
+++ unit_converter.py
@@ -0,0 +1,9 @@
+kilometers=float(input("enter kilometers"))
+miles=kilometers*0.621371
+
+print(f'{kilometers} km = {miles:.2f} miles')
+
+temp_c =float(input("enter celsius"))
+temp_f =(temp_c*9/5)+ 32
+
+print(f'{temp_c} c = {temp_f:.1f} f')
